67ea5fbd923547f2d80b970aac0ce0bb44b65ea9
Jakub Biesiada <biesiadajakub@icloud.com>
2018-10-25 19:39:47 +0200

    Repository config

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env;
+
+let libraryName = 'monument-ui';
+
+let outputFile, mode, devtool;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+  devtool = false;
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+  devtool = 'source-map';
+}
+
+const config = {
+  mode,
+  entry: __dirname + '/src/index.js',
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  },
+  devtool,
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'MonumentUI',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;
