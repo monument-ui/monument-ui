213ffeaaa813ca92b121f462da4f98eabdd018ff
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-23 22:09:49 +0100

    Update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-links', '@storybook/addon-knobs']
+  addons: ['@storybook/addon-links', '@storybook/addon-knobs'],
 };
